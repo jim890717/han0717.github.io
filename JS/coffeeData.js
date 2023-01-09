881b8833b6b187edee4c213198b251e9e6921494
han0717 <nayeon94520@gmail.com>
2023-01-09 15:13:26 +0800

    upload coffee_html

---
+++ JS/coffeeData.js
@@ -0,0 +1,34 @@
+var foodData=
+[
+    {
+        "id":1,
+        "area":"Taipei",
+        "name":"早初entry time (早午餐)",
+        "pictPath":"./pict/coffee/Taipei/1.jpg",
+        "location":"台北市萬華區成都路165號",
+        "location_href":"https://goo.gl/maps/Tp4qkXTPStj9UEwSA",
+        "date":"一至五10:00-16:30/六、日延長半小時｜週三休",
+        "detailText":""
+    },
+    {
+        "id":2,
+        "area":"Taipei",
+        "name":"CAFE RACO",
+        "pictPath":"./pict/coffee/Taipei/2.jpg",
+        "location":"台北市中山區民生東路二段147巷1號號1樓",        
+        "location_href":"https://goo.gl/maps/uxgMEwNdxmxUvJkQA",
+        "date":"每日 10:00-21:00",
+        "detailText":"不限時享受甜點與咖啡的美好"
+    },
+    {
+        "id":3,
+        "area":"Taipei",
+        "name":"La Chaudière Coffee 樂初咖啡",
+        "pictPath":"./pict/coffee/Taipei/3.jpg",
+        "location":"台北市松山區復興北路361巷6號",        
+        "location_href":"https://goo.gl/maps/9ELJwWKSN7nQwxzD7",
+        "date":"每日 09:00-19:00",
+        "detailText":"爆漿甜甜圈"
+    }
+
+]
