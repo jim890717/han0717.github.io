acc2478be6d3c89e7c4d1967ba60b9796f1b3978
han0717 <nayeon94520@gmail.com>
2023-01-09 17:03:03 +0800

    20230109V2

--- JS/coffeeData.js
+++ JS/coffeeData.js
@@ -1,8 +1,9 @@
 var foodData=
 [
     {
-        "id":1,
+        //"id":1,
         "area":"Taipei",
+        "region":"萬華區",
         "name":"早初entry time (早午餐)",
         "pictPath":"./pict/coffee/Taipei/1.jpg",
         "location":"台北市萬華區成都路165號",
@@ -11,9 +12,9 @@ var foodData=
         "detailText":""
     },
     {
-        "id":2,
         "area":"Taipei",
         "name":"CAFE RACO",
+        "region":"中山區",
         "pictPath":"./pict/coffee/Taipei/2.jpg",
         "location":"台北市中山區民生東路二段147巷1號號1樓",        
         "location_href":"https://goo.gl/maps/uxgMEwNdxmxUvJkQA",
@@ -21,14 +22,64 @@ var foodData=
         "detailText":"不限時享受甜點與咖啡的美好"
     },
     {
-        "id":3,
         "area":"Taipei",
         "name":"La Chaudière Coffee 樂初咖啡",
+        "region":"松山區",
         "pictPath":"./pict/coffee/Taipei/3.jpg",
         "location":"台北市松山區復興北路361巷6號",        
         "location_href":"https://goo.gl/maps/9ELJwWKSN7nQwxzD7",
         "date":"每日 09:00-19:00",
         "detailText":"爆漿甜甜圈"
+    },
+    {
+        "area":"Taipei",
+        "name":"八時神仙草",
+        "region":"中山區",
+        "pictPath":"./pict/coffee/Taipei/4.jpg",
+        "location":"台北市中山區中山北路一段135巷5號",        
+        "location_href":"https://goo.gl/maps/iyW7E1XetuXPb18W8",
+        "date":"每日 12:30–21:00",
+        "detailText":""
+    },
+    {
+        "area":"Taipei",
+        "name":"暖男炸雞",
+        "region":"信義區",
+        "pictPath":"./pict/coffee/Taipei/5.jpg",
+        "location":"台北市信義區永吉路30巷122號旁的空地",        
+        "location_href":"https://goo.gl/maps/nJB3LwgoGXEZQLTx7",
+        "date":"每日 15:00–23:30",
+        "detailText":""
+    },
+    {
+        "area":"newTaipei",
+        "name":"吉利菓子",
+        "region":"淡水區",
+        "pictPath":"./pict/coffee/NewTaipei/1.jpg",
+        "location":"新北市淡水區中正路264號馬偕街旁邊",        
+        "location_href":"https://goo.gl/maps/8ZeATMVzxiFua9Af8",
+        "date":"每日 12:00-20:30",
+        "detailText":""
+    },
+    {
+        "area":"taichung",
+        "name":"解藥糖果屋",
+        "region":"北區",
+        "pictPath":"./pict/coffee/TaiChung/1.jpg",
+        "location":"台中市北區太平路75巷16號",        
+        "location_href":"https://goo.gl/maps/x5nGXTndHpwgbLm1A",
+        "date":"每日 09:00-19:00",
+        "detailText":""
+    },
+    {
+        "area":"tainan",
+        "name":"Mr.Z 烘焙小棧 (蛋塔、布丁)",
+        "region":"中西區",
+        "pictPath":"./pict/coffee/Tainan/1.jpg",
+        "location":"台南市中西區海安路二段118號",        
+        "location_href":"https://goo.gl/maps/xW2CXjFiwpNjbQQh6",
+        "date":"12:00-21:00｜週三、四休",
+        "detailText":""
     }
 
 ]
