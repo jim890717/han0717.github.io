e22955b8c70763cf4dcd06598c2d14c09ca265aa
han0717 <nayeon94520@gmail.com>
2023-01-11 14:18:19 +0800

    2023011

--- JS/coffeeData.js
+++ JS/coffeeData.js
@@ -80,6 +80,93 @@ var foodData=
         "location_href":"https://goo.gl/maps/xW2CXjFiwpNjbQQh6",
         "date":"12:00-21:00｜週三、四休",
         "detailText":""
-    }
+    },
+    {
+        "area":"Taipei",
+        "name":"TankQ Cafe & Bar",
+        "region":"中山區",
+        "pictPath":"./pict/coffee/Taipei/6.jpg",
+        "location":"台北市中山區松江路90巷3-2號",        
+        "location_href":"https://goo.gl/maps/xfHaGA9obHmmBd216",
+        "date":"每日 11:00-22:00",
+        "detailText":""
+    },
+    {
+        "area":"Taipei",
+        "name":"別處咖啡 Away cafe",
+        "region":"中山區",
+        "pictPath":"./pict/coffee/Taipei/8.jpg",
+        "location":"台北市中正區羅斯福路三段244巷9弄2-1號",        
+        "location_href":"https://goo.gl/maps/MhudYek8nStF41Mj6",
+        "date":"平日 13:00-21:00|六日 11:00-22:00",
+        "detailText":"別處咖啡。可以畫畫又有貓陪伴的咖啡館"
+    },
+    {
+        "area":"Taipei",
+        "name":"室香Savour Kaffee",
+        "region":"信義區",
+        "pictPath":"./pict/coffee/Taipei/7.jpg",
+        "location":"台北市信義區吳興街345巷10號1樓",        
+        "location_href":"https://goo.gl/maps/NH3bo16jkUt6eRab6",
+        "date":"13:00-19:00|二、三、四公休",
+        "detailText":""
+    },
+    {
+        "area":"Taipei",
+        "name":"記憶的溫度",
+        "region":"北投區",
+        "pictPath":"./pict/coffee/Taipei/9.jpg",
+        "location":"台北市北投區珠海路38號",        
+        "location_href":"https://goo.gl/maps/Pawvg9zeoJRPgZuS8",
+        "date":"11:30-14:00|17:30-20:30",
+        "detailText":""
+    },
+    {
+        "area":"Taipei",
+        "name":"妳有咖啡 neo cafe",
+        "region":"萬華區",
+        "pictPath":"./pict/coffee/Taipei/10.jpg",
+        "location":"台北市萬華區武昌街二段83-9號號 2F",        
+        "location_href":"https://goo.gl/maps/T9RS32FtWsSLyQ4A7",
+        "date":"平日 11:30-21:45|五六日 11:30-23:30",
+        "detailText":""
+    },
+    {
+        "area":"Taipei",
+        "name":"貓食光 Time With Cats",
+        "region":"大安區",
+        "pictPath":"./pict/coffee/Taipei/11.jpg",
+        "location":"台北市大安區羅斯福路三段297-1號1樓",        
+        "location_href":"https://goo.gl/maps/12EHU7hFVnwKWCw66",
+        "date":"12:00-20:30|一、二公休",
+        "detailText":""
+    },{
+        "area":"Taipei",
+        "name":"卜卜商店",
+        "region":"大同區",
+        "pictPath":"./pict/coffee/Taipei/12.jpg",
+        "location":"台北市大同區赤峰街49巷2號2樓",        
+        "location_href":"https://goo.gl/maps/Y6TT4YDZAnAqfvp69",
+        "date":"平日 13:00-19:00|六日 11:00-19:30|二公休",
+        "detailText":""
+    },{
+        "area":"Taipei",
+        "name":"好啊 Cafe",
+        "region":"大同區",
+        "pictPath":"./pict/coffee/Taipei/13.jpg",
+        "location":"台北市大同區萬全街46號",        
+        "location_href":"https://goo.gl/maps/mX4jXXUuzV9BeWYA6",
+        "date":"每日 12:00-19:30|日公休",
+        "detailText":""
+    },{
+        "area":"Taipei",
+        "name":"和貓咪有約",
+        "region":"松山區",
+        "pictPath":"./pict/coffee/Taipei/14.jpg",
+        "location":"台北市松山區三民路3巷21號",        
+        "location_href":"https://goo.gl/maps/7uWXZCdtXucUAz4h9",
+        "date":"每日 11:00-20:00",
+        "detailText":""
+    },
 
 ]
